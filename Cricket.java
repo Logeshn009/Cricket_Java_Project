c3494abc1000c43345ca5d9d8f970d427d1be686
Logesh N <95091107+Logeshn009@users.noreply.github.com>
2022-03-23 12:56:05 +0530

    Add files via upload

---
+++ Cricket.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Cricket{
+    public static void main(String[] args){
+    
+
+        
+        Admin ad = new Admin();
+        Player pl = new Player();
+        Manager mn = new Manager();
+        Coach ch = new Coach();
+        Team tm = new Team();
+
+        ad.Admin 
+    }
+}
+
+
+class Admin{
+
+     int admin_id;
+     String admin_name, admin_email, admin_Phone, admin_address;
+
+public void admin_details(int customer_id, String admin_name, String admin_email, String admin_Phone, String admin_address)
+{
+    this.admin_id = admin_id;
+    this.admin_name = admin_name;
+    this.admin_email = admin_email;
+    this.admin_Phone = admin_Phone;
+    this.admin_address = admin_address;
+}
+public void admin_details_display(){
+    System.out.println("THE FOLLOWIG OUTPUT SHOWS ALL POSSILE CLASSES THAT ARE USED IN BANKING DOMAIN WITH SOME VALUES");
+    System.out.println("\n The Inputs are static input passed from objects of respective classes");
+
+    System.out.println("----- Admin Details -----");
+    
+    System.out.println("\nAdmin Name: ");
+}
+   
+}
+
+class Player{
+
+  
+}
+
+class Manager{
+
+    
+}
+
+class Coach{
+
+
+}
+
+class Team{
+
+    
+}
