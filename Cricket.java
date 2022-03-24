8c0ae7e65ec75435f2e647a6322c83d2d56d862d
Logesh N <95091107+Logeshn009@users.noreply.github.com>
2022-03-24 13:58:59 +0530

    Add files via upload

--- Cricket.java
+++ Cricket.java
@@ -1,61 +1,63 @@
 import java.util.Scanner;
 
-public class Cricket{
-    public static void main(String[] args){
-    
+public class Cricket {
+    public static void main(String[] args) {
 
-        
         Admin ad = new Admin();
         Player pl = new Player();
         Manager mn = new Manager();
         Coach ch = new Coach();
         Team tm = new Team();
 
-        ad.Admin 
+        ad.admin_details(100, "Logesh N", "logeshnandhakumar12@gmail.com", "7548817148",
+                "D/N - 5, durga nagar, zuzuvadi, Hosur.");
+
+        ad.admin_details_display();
     }
 }
 
+class Admin {
 
-class Admin{
+    int admin_id;
+    String admin_name, admin_email, admin_Phone, admin_address;
 
-     int admin_id;
-     String admin_name, admin_email, admin_Phone, admin_address;
+    public void admin_details(int adminze_id, String admin_name, String admin_email, String admin_Phone,
+            String admin_address) {
+        this.admin_id = admin_id;
+        this.admin_name = admin_name;
+        this.admin_email = admin_email;
+        this.admin_Phone = admin_Phone;
+        this.admin_address = admin_address;
+    }
 
-public void admin_details(int customer_id, String admin_name, String admin_email, String admin_Phone, String admin_address)
-{
-    this.admin_id = admin_id;
-    this.admin_name = admin_name;
-    this.admin_email = admin_email;
-    this.admin_Phone = admin_Phone;
-    this.admin_address = admin_address;
-}
-public void admin_details_display(){
-    System.out.println("THE FOLLOWIG OUTPUT SHOWS ALL POSSILE CLASSES THAT ARE USED IN BANKING DOMAIN WITH SOME VALUES");
-    System.out.println("\n The Inputs are static input passed from objects of respective classes");
+    public void admin_details_display() {
+        System.out.println(
+                "THE FOLLOWIG OUTPUT SHOWS ALL POSSILE CLASSES THAT ARE USED IN BANKING DOMAIN WITH SOME VALUES");
+        System.out.println("\n The Inputs are static input passed from objects of respective classes");
 
-    System.out.println("----- Admin Details -----");
-    
-    System.out.println("\nAdmin Name: ");
-}
-   
-}
+        System.out.println("----- Admin Details -----");
 
-class Player{
-
-  
-}
-
-class Manager{
-
-    
-}
-
-class Coach{
-
+        System.out.println("\nAdmin id: " + admin_id);
+        System.out.println("Admin Name: " + admin_name);
+        System.out.println("Admin email: " + admin_email);
+        System.out.println("Admin Phone: " + admin_Phone);
+        System.out.println("Admin Address: " + admin_address);
+    }
 
 }
 
-class Team{
+class Player {
 
-    
+}
+
+class Manager {
+
+}
+
+class Coach {
+
+}
+
+class Team {
+
 }
